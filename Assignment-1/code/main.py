3a9feea14bde1f60c8c3fff45694425f8e527239
Vishal Devadiga <vishaldevadiga003@gmail.com>
2022-03-30 16:09:41 +0530

    2nd Iteration of Assignment 1

--- Assignment-1/code/main.py
+++ Assignment-1/code/main.py
@@ -2,12 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy import linalg as LA
 
-def line_gen(bool_line,*points):
+def line_gen(type_of_line,*points):
     arr_x = [p[0] for p in points]
     arr_y = [p[1] for p in points]
-    if bool_line == "points":
+    if type_of_line == "points":
         plt.plot(arr_x,arr_y,'o')
-    elif bool_line == "lines":
+    elif type_of_line == "lines":
         plt.plot(arr_x,arr_y,marker = 'o')
 
 def centroid(*points):
@@ -19,19 +19,21 @@ def centroid(*points):
     return [centroid_x, centroid_y]
 
 def line_slope(p1,p2):
-    slope = (p2[1] - p1[1])/(p2[0] - p1[0])
+    diff_points = p2 - p1
+    slope = diff_points[1] / diff_points[0]
     return slope
 
 A = np.array([-1, 3])
 B = np.array([4, 2])
 C = np.array([3, -2])
+O = np.array([0,0])
 
 # Finding centroid
 G = np.array(centroid(A,B,C))
 
-# Draw the traingle and centroid
+# Draw the triangle and centroid,origin
 line_gen("lines",A,B,C,A)
-line_gen("points",G)
+line_gen("points",G,O)
 
 # Draw Line parallel to AC through G
 plt.axline(G , slope = line_slope(A,C))
@@ -44,6 +46,7 @@ plt.annotate("A",A,xytext=(0,5),textcoords="offset points")
 plt.annotate("B",B,xytext=(0,5),textcoords="offset points")
 plt.annotate("C",C,xytext=(0,5),textcoords="offset points")
 plt.annotate("G",G,xytext=(0,5),textcoords="offset points")
+plt.annotate("O",O,xytext=(0,5),textcoords="offset points")
 plt.grid()
 
 plt.savefig("../figs/9.2.png")
